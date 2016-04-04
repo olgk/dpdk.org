olgk/dpdk.org

commit c83fd65e15a8a08c810004e02a985169274bf32a

mlx5: add support for send inline
Implement send inline feature which copy the packet
into the wqe.
The size of packet that will proccessed as inline packet,
can be configured by user.
This feature become effective when the HW is the bottle-neck.

Signed-off-by: Yaacov Hazan <yaacovh@mellanox.com>

drivers/net/mlx5/mlx5_rxtx.c
@@ -150,6 +150,7 @@ txq_complete_compressed(struct ftxq *txq, volatile struct mlx5_cqe64 *cqe,
 	return wqe_ci;
 }
 
+#if MLX5_PMD_MAX_INLINE == 0
 /**
  * Manage TX completions.
  *
@@ -215,6 +216,65 @@ txq_complete(struct ftxq *txq)
 	*txq->cq_db = htonl(txq->cq_ci);
 }
 
+#else /* MLX5_PMD_MAX_INLINE == 0 */
+
+static void
+txq_complete(struct ftxq *txq)
+{
+	const unsigned int elts_n = txq->elts_n;
+	const unsigned int cqe_n = MLX5_TX_CQ_SIZE - 1;
+	uint16_t elts_free = txq->elts_tail;
+	uint16_t elts_tail = txq->elts_tail;
+	uint16_t cq_ci = txq->cq_ci;
+	unsigned int npolled = 0;
+	int ret = 0;
+
+	while (ret == 0) {
+		unsigned int idx = cq_ci & cqe_n;
+		volatile struct mlx5_cqe64 *cqe = &(*txq->cqes)[idx];
+
+		ret = check_cqe64(cqe, MLX5_TX_CQ_SIZE, cq_ci);
+		if (ret == 1)
+			break;
+
+		if (MLX5_CQE_FORMAT(cqe->op_own) == MLX5_COMPRESSED)
+			npolled += ntohl(cqe->byte_cnt);
+		else if ((MLX5_CQE_OPCODE(cqe->op_own) == MLX5_CQE_REQ)) {
+			npolled++;
+			++cq_ci;
+		}
+	}
+	if (unlikely(npolled == 0))
+		return;
+	/* Free buffers. */
+	elts_tail += npolled * txq->elts_comp_cd_init;
+	if (elts_tail >= elts_n)
+		elts_tail -= elts_n;
+	while (elts_free != elts_tail) {
+		struct rte_mbuf *elt = (*txq->elts)[elts_free];
+		unsigned int elts_free_next =
+			(elts_free + 1) & (elts_n - 1);
+		struct rte_mbuf *elt_next = (*txq->elts)[elts_free_next];
+
+		(*txq->elts)[elts_free] = NULL;
+		RTE_MBUF_PREFETCH_TO_FREE(elt_next);
+		/* Faster than rte_pktmbuf_free(). */
+		do {
+			struct rte_mbuf *next = NEXT(elt);
+
+			rte_pktmbuf_free_seg(elt);
+			elt = next;
+		} while (elt != NULL);
+		elts_free = elts_free_next;
+	}
+	txq->cq_ci = cq_ci;
+	txq->elts_tail = elts_tail;
+	/* Update the consumer index. */
+	rte_wmb();
+	*txq->cq_db = htonl(txq->cq_ci);
+}
+#endif /* MLX5_PMD_MAX_INLINE == 0 */
+
 /**
  * Get Memory Pool (MP) from mbuf. If mbuf is indirect, the pool from which
  * the cloned mbuf is allocated is returned instead.
@@ -263,7 +323,8 @@ txq_mp2mr(struct ftxq *txq, const struct rte_mempool *mp)
 		}
 		if (txq->mp2mr[i].mp == mp) {
 			assert(txq->mp2mr[i].lkey != (uint32_t)-1);
-			assert(htonl((txq->mp2mr[i].mr->lkey) == txq->mp2mr[i].lkey);
+			assert(htonl(txq->mp2mr[i].mr->lkey) ==
+			       txq->mp2mr[i].lkey);
 			lkey = txq->mp2mr[i].lkey;
 			break;
 		}
@@ -274,6 +335,7 @@ txq_mp2mr(struct ftxq *txq, const struct rte_mempool *mp)
 	return lkey;
 }
 
+#if MLX5_PMD_MAX_INLINE == 0
 static inline void
 mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	       uintptr_t addr, uint32_t length, uint32_t lkey)
@@ -297,6 +359,94 @@ mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	/* Increase the consumer index. */
 	++txq->wqe_ci;
 }
+
+#else /* MLX5_PMD_MAX_INLINE == 0 */
+
+static inline void
+mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
+	       uintptr_t addr, uint32_t length, uint32_t lkey)
+{
+	/* Copy the first 16 bytes into the inline header */
+	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start,
+	       (void *)(uintptr_t)addr,
+	       MLX5_ETH_INLINE_HEADER_SIZE);
+	addr += MLX5_ETH_INLINE_HEADER_SIZE;
+	length -= MLX5_ETH_INLINE_HEADER_SIZE;
+
+	wqe->dseg.byte_count = htonl(length);
+	wqe->dseg.lkey = lkey;
+	wqe->dseg.addr = htonll(addr);
+
+	wqe->ctrl.data[0] = htonl((txq->wqe_ci << 8) | MLX5_OPCODE_SEND);
+	wqe->ctrl.data[1] = htonl((txq->qp_num << 8) | 4);
+	if (unlikely(--txq->elts_comp == 0)) {
+		wqe->ctrl.data[2] = htonl(8);
+		txq->elts_comp = txq->elts_comp_cd_init;
+	} else
+		wqe->ctrl.data[2] = htonl(0);
+	wqe->ctrl.data[3] = 0;
+
+	/* Increase the consumer index. */
+	++txq->wqe_ci;
+}
+
+static inline void
+mlx5_wqe_write_inline(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
+		      uintptr_t addr, uint32_t length)
+{
+	uint32_t size;
+	uint32_t wqes_cnt = 1;
+	volatile struct mlx5_wqe64_inl *inl_wqe =
+		(volatile struct mlx5_wqe64_inl *)wqe;
+
+	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_INLINE_HEADER_SIZE);
+	/* Copy the first 16 bytes into the inline header */
+	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start,
+	       (void *)(uintptr_t)addr,
+	       MLX5_ETH_INLINE_HEADER_SIZE);
+	addr += MLX5_ETH_INLINE_HEADER_SIZE;
+	length -= MLX5_ETH_INLINE_HEADER_SIZE;
+
+	size = (sizeof(wqe->ctrl.ctrl) +
+		sizeof(wqe->eseg) +
+		sizeof(wqe->dseg.byte_count) +
+		length + 15) / 16;
+
+	wqe->dseg.byte_count = htonl(length | MLX5_INLINE_SEG);
+	memcpy((void *)(uintptr_t)&inl_wqe->wqe.data[MLX5_WQE64_INL_DATA_OFFSET],
+	       (void *)addr, MLX5_WQE64_INL_DATA);
+	addr += MLX5_WQE64_INL_DATA;
+	length -= MLX5_WQE64_INL_DATA;
+
+	while (length) {
+		volatile struct mlx5_wqe64_inl *wqe_next =
+			(volatile struct mlx5_wqe64_inl *)
+			&(*txq->wqes)[(txq->wqe_ci + wqes_cnt) &
+				      (txq->wqe_cnt - 1)];
+		uint32_t copy_bytes = (length > sizeof(*wqe)) ?
+				      sizeof(*wqe) :
+				      length;
+
+		memcpy((void *)(uintptr_t)wqe_next->wqe.data, (void *)addr,
+		       copy_bytes);
+		addr += copy_bytes;
+		length -= copy_bytes;
+		wqes_cnt++;
+	}
+
+	wqe->ctrl.data[0] = htonl((txq->wqe_ci << 8) | MLX5_OPCODE_SEND);
+	wqe->ctrl.data[1] = htonl((txq->qp_num << 8) | (size & 0x3f));
+	if (unlikely(--txq->elts_comp == 0)) {
+		wqe->ctrl.data[2] = htonl(8);
+		txq->elts_comp = txq->elts_comp_cd_init;
+	} else
+		wqe->ctrl.data[2] = htonl(0);
+	wqe->ctrl.data[3] = 0;
+
+	/* Increase the consumer index. */
+	txq->wqe_ci += wqes_cnt;
+}
+#endif /* MLX5_PMD_MAX_INLINE == 0 */
 
 /*
  * Avoid using memcpy() to copy to BlueFlame page, since memcpy()
@@ -405,7 +555,7 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		uint32_t lkey;
 		uintptr_t buf_next_addr;
 
-		wqe = &(*txq->wqes)[elts_head];
+		wqe = &(*txq->wqes)[txq->wqe_ci &  (txq->wqe_cnt - 1)];
 		rte_prefetch0(wqe);
 		if (i + 1 < max)
 			rte_prefetch0(buf_next);
@@ -435,9 +585,16 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 			rte_prefetch0((volatile void *)
 				      (uintptr_t)buf_next_addr);
 		}
-		/* Retrieve Memory Region key for this memory pool. */
-		lkey = txq_mp2mr(txq, txq_mb2mp(buf));
-		mlx5_wqe_write(txq, wqe, addr, length, lkey);
+#if MLX5_PMD_MAX_INLINE > 0
+		if (length <= MLX5_PMD_MAX_INLINE)
+			mlx5_wqe_write_inline(txq, wqe, addr, length);
+		else
+#endif
+		{
+			/* Retrieve Memory Region key for this memory pool. */
+			lkey = txq_mp2mr(txq, txq_mb2mp(buf));
+			mlx5_wqe_write(txq, wqe, addr, length, lkey);
+		}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 			sent_size += length;
 #endif

config/common_bsdapp
@@ -224,6 +224,7 @@ CONFIG_RTE_LIBRTE_MLX4_SOFT_COUNTERS=1
 #
 CONFIG_RTE_LIBRTE_MLX5_PMD=n
 CONFIG_RTE_LIBRTE_MLX5_DEBUG=n
+CONFIG_RTE_LIBRTE_MLX5_MAX_INLINE=0
 CONFIG_RTE_LIBRTE_MLX5_TX_MP_CACHE=8
 
 #

config/common_linuxapp
@@ -223,6 +223,7 @@ CONFIG_RTE_LIBRTE_MLX4_SOFT_COUNTERS=1
 #
 CONFIG_RTE_LIBRTE_MLX5_PMD=n
 CONFIG_RTE_LIBRTE_MLX5_DEBUG=n
+CONFIG_RTE_LIBRTE_MLX5_MAX_INLINE=0
 CONFIG_RTE_LIBRTE_MLX5_TX_MP_CACHE=8
 
 #

drivers/net/mlx5/Makefile
@@ -90,6 +90,10 @@ else
 CFLAGS += -DNDEBUG -UPEDANTIC
 endif
 
+ifdef CONFIG_RTE_LIBRTE_MLX5_MAX_INLINE
+CFLAGS += -DMLX5_PMD_MAX_INLINE=$(CONFIG_RTE_LIBRTE_MLX5_MAX_INLINE)
+endif
+
 ifdef CONFIG_RTE_LIBRTE_MLX5_TX_MP_CACHE
 CFLAGS += -DMLX5_PMD_TX_MP_CACHE=$(CONFIG_RTE_LIBRTE_MLX5_TX_MP_CACHE)
 endif

drivers/net/mlx5/mlx5_defs.h
@@ -57,6 +57,11 @@
 /* TX Completion Queue size. */
 #define MLX5_TX_CQ_SIZE 16
 
+/* Maximum size for inline data. */
+#ifndef MLX5_PMD_MAX_INLINE
+#define MLX5_PMD_MAX_INLINE 0
+#endif
+
 /*
  * Maximum number of cached Memory Pools (MPs) per TX queue. Each RTE MP
  * from which buffers are to be transmitted will have to be mapped by this

drivers/net/mlx5/mlx5_rxtx.h
@@ -231,14 +231,26 @@ struct mlx5_wqe64 {
 	struct mlx5_wqe_data_seg dseg;
 };
 
+#define MLX5_WQE64_INL_DATA 12
+#define MLX5_WQE64_INL_DATA_OFFSET 52
+
+struct mlx5_wqe64_inl {
+	union {
+		struct mlx5_wqe64 wqe;
+		char data[64];
+	} wqe;
+};
+
 struct ftxq {
 	uint16_t elts_head; /* Current index in (*elts)[]. */
 	uint16_t elts_tail; /* First element awaiting completion. */
 	uint16_t elts_comp_cd_init; /* Initial value for countdown. */
 	uint16_t elts_comp_npr; /* number of completion per ring. */
+	uint16_t elts_comp; /* number of completion per ring. */
 	uint16_t elts_n;
 	uint16_t cq_ci;
 	uint16_t wqe_ci;
+	uint16_t wqe_cnt;
 	uint16_t bf_offset;
 	uint16_t bf_buf_size;
 	volatile struct mlx5_cqe64 (*cqes)[MLX5_TX_CQ_SIZE];

drivers/net/mlx5/mlx5_txq.c
@@ -80,10 +80,12 @@ txq_alloc_elts(struct txq *txq, unsigned int elts_n)
 	unsigned int i;
 	unsigned int comp = txq->ftxq.elts_comp_cd_init;
 
-	for (i = 0; (i != elts_n); ++i) {
+	for (i = 0; (i != elts_n); ++i)
+		(*txq->ftxq.elts)[i] = NULL;
+
+	for (i = 0; (i != txq->ftxq.wqe_cnt); ++i) {
 		volatile struct mlx5_wqe64 *wqe = &(*txq->ftxq.wqes)[i];
 
-		(*txq->ftxq.elts)[i] = NULL;
 		memset((void *)(uintptr_t)wqe, 0, sizeof(struct mlx5_wqe64));
 		wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_INLINE_HEADER_SIZE);
 		wqe->ctrl.data[1] = htonl((txq->ftxq.qp_num << 8) | 4);
@@ -98,6 +100,7 @@ txq_alloc_elts(struct txq *txq, unsigned int elts_n)
 	DEBUG("%p: allocated and configured %u WRs", (void *)txq, elts_n);
 	txq->ftxq.elts_head = 0;
 	txq->ftxq.elts_tail = 0;
+	txq->ftxq.elts_comp = txq->ftxq.elts_comp_cd_init;
 }
 
 /**
@@ -209,6 +212,7 @@ txq_ftxq_setup(struct txq *tmpl, struct txq *txq)
 	tmpl->ftxq.wqes =
 		(volatile struct mlx5_wqe64 (*)[])
 		(uintptr_t)qp->gen_data.sqstart;
+	tmpl->ftxq.wqe_cnt = qp->sq.wqe_cnt;
 	tmpl->ftxq.qp_db = &qp->gen_data.db[MLX5_SND_DBR];
 	tmpl->ftxq.bf_reg = qp->gen_data.bf->reg;
 	tmpl->ftxq.bf_offset = qp->gen_data.bf->offset;
@@ -304,6 +308,9 @@ txq_setup(struct rte_eth_dev *dev, struct txq *txq, uint16_t desc,
 					desc),
 			/* Max number of scatter/gather elements in a WR. */
 			.max_send_sge = 1,
+#if MLX5_PMD_MAX_INLINE > 0
+			.max_inline_data = MLX5_PMD_MAX_INLINE,
+#endif
 		},
 		.qp_type = IBV_QPT_RAW_PACKET,
 		/* Do *NOT* enable this, completions events are managed per

commit bc44d90996d4ac36a057fab82b53199bf1f04507

mlx5: fix write wqe functions in inline mode
Reset wqe->eseg fields in inline mode, since they may
contain some data from previous used wqes.

Fixes: c83fd65 ("mlx5: add support for send inline")

Signed-off-by: Yaacov Hazan <yaacovh@mellanox.com>

drivers/net/mlx5/mlx5_rxtx.c
@@ -449,6 +449,14 @@ static inline void
 mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	       uintptr_t addr, uint32_t length, uint32_t lkey)
 {
+	wqe->eseg = (struct mlx5_wqe_eth_seg) {
+		.rsvd0 = 0,
+		.cs_flags = 0,
+		.rsvd1 = 0,
+		.mss = 0,
+		.rsvd2 = 0,
+	};
+
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_INLINE_HEADER_SIZE);
 	/* Copy the first 16 bytes into the inline header */
 	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start,
@@ -479,6 +487,14 @@ mlx5_wqe_write_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 		    uintptr_t addr, uint32_t length, uint32_t lkey,
 		    uint16_t vlan_tci)
 {
+	wqe->eseg = (struct mlx5_wqe_eth_seg) {
+		.rsvd0 = 0,
+		.cs_flags = 0,
+		.rsvd1 = 0,
+		.mss = 0,
+		.rsvd2 = 0,
+	};
+
 	uint32_t vlan = htonl(0x81000000 | vlan_tci);
 
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
@@ -525,6 +541,14 @@ mlx5_wqe_write_inline(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	uint32_t wqes_cnt = 1;
 	volatile struct mlx5_wqe64_inl *inl_wqe =
 		(volatile struct mlx5_wqe64_inl *)wqe;
+
+	wqe->eseg = (struct mlx5_wqe_eth_seg) {
+		.rsvd0 = 0,
+		.cs_flags = 0,
+		.rsvd1 = 0,
+		.mss = 0,
+		.rsvd2 = 0,
+	};
 
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_INLINE_HEADER_SIZE);
 	/* Copy the first 16 bytes into the inline header */
@@ -583,6 +607,14 @@ mlx5_wqe_write_inline_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	uint32_t vlan = htonl(0x81000000 | vlan_tci);
 	volatile struct mlx5_wqe64_inl *inl_wqe =
 		(volatile struct mlx5_wqe64_inl *)wqe;
+
+	wqe->eseg = (struct mlx5_wqe_eth_seg) {
+		.rsvd0 = 0,
+		.cs_flags = 0,
+		.rsvd1 = 0,
+		.mss = 0,
+		.rsvd2 = 0,
+	};
 
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
 	/*